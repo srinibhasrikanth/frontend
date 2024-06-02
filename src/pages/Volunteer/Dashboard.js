55d5b6ef100ac523a118b54da9b1d0d25cd23940
srinibhasrikanth <srinibha.srikanth@gmail.com>
2024-06-02 17:29:38 +0530

    send emails

--- src/pages/Volunteer/Dashboard.js
+++ src/pages/Volunteer/Dashboard.js
@@ -1,9 +1,16 @@
+// Dashboard.js
 import React from "react";
-
 import EventCard from "./EventCard";
-import { Typography } from "@mui/material";
+import { useAuth } from "../../context/auth";
+import { Redirect } from "react-router-dom"; // Ensure you have react-router-dom installed
 
 const Dashboard = () => {
+  const { auth } = useAuth();
+
+  if (!auth.token) {
+    return <Redirect to="/login" />; // Redirect to login page if not authenticated
+  }
+
   return (
     <div>
       <h2
@@ -11,9 +18,9 @@ const Dashboard = () => {
           fontFamily: "Poppins",
           fontWeight: 400,
           fontStyle: "normal",
-          textAlign:"center",
-          margin:4,
-          fontSize:"30px"
+          textAlign: "center",
+          margin: 4,
+          fontSize: "30px",
         }}
       >
         Welcome to Volunteer Dashboard.
